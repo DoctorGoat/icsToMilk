e2552b88fa86ee6dda7fc88b5e8f99fb52e533fd
thearcy@gmail.com <thearcy@gmail.com>
2017-03-14 17:09:45 -0400

    First version but functional this time!

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using Ical.Net;
+using Ical.Net.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace icsToMilk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void icsParse(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            OpenFileDialog icsReader = new OpenFileDialog();
+            icsReader.InitialDirectory = Environment.CurrentDirectory;
+            if (icsReader.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    IICalendarCollection calendarCollection = Calendar.LoadFromFile(icsReader.FileName);
+                    foreach (Calendar c in calendarCollection)
+                    {
+                        foreach (Event eve in c.Events)
+                        {
+                            //As far as I can tell, the first category is always the class name. This is what we want.
+                            textBox1.AppendText(eve.Categories[0] + ": " + eve.Summary + " by " + eve.DtEnd + Environment.NewLine);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * This program exists to convert ICS calendar files (Moodle's, specifically) into a format that can be plugged into Remember the Milk.
+ * by Curtis Moyle, to make things at GTCC more managable.
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ical.Net;
+
+namespace icsToMilk
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+    }
+}
